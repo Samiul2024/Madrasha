a88bb32f3c194ae1981a83cfa615d9792e90395c
Samiul2024 <mdsamiullahossen@gmail.com>
2026-05-23 21:41:40 +0600

    refactor(router): merge public routes with protected dashboard layout

--- src/app/router/AppRouter.jsx
+++ src/app/router/AppRouter.jsx
@@ -1,38 +1,60 @@
 import { Routes, Route } from "react-router-dom";
 
+/* =========================
+   LAYOUTS
+========================= */
+import MainLayout from "../../layouts/MainLayout";
+
+import DashboardLayout from "../../features/dashboard/layouts/DashboardLayout";
+import ProtectedRoute from "../../features/auth/ProtectedRoute";
+
+/* =========================
+   PUBLIC PAGES
+========================= */
 import HomePage from "../../pages/public/HomePage";
 import AboutPage from "../../pages/public/AboutPage";
 import CoursesPage from "../../pages/public/CoursesPage";
 import AdmissionPage from "../../pages/public/AdmissionPage";
 import NoticesPage from "../../pages/public/NoticesPage";
 import GalleryPage from "../../pages/public/GalleryPage";
-import ContactPage from "../../pages/public/ContactPage";
 import DonationPage from "../../pages/public/DonationPage";
+import ContactPage from "../../pages/public/ContactPage";
 
+/* =========================
+   DASHBOARD PAGES
+========================= */
 import AdminLoginPage from "../../features/dashboard/pages/AdminLoginPage";
 import DashboardHomePage from "../../features/dashboard/pages/DashboardHomePage";
-
-import DashboardLayout from "../../features/dashboard/layouts/DashboardLayout";
-import ProtectedRoute from "../../features/auth/ProtectedRoute";
 
 const AppRouter = () => {
   return (
     <Routes>
 
-      {/* PUBLIC ROUTES */}
-      <Route path="/" element={<HomePage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/courses" element={<CoursesPage />} />
-      <Route path="/admission" element={<AdmissionPage />} />
-      <Route path="/notices" element={<NoticesPage />} />
-      <Route path="/gallery" element={<GalleryPage />} />
-      <Route path="/contact" element={<ContactPage />} />
-      <Route path="/donation" element={<DonationPage />} />
+      {/* =========================
+          PUBLIC WEBSITE ROUTES
+      ========================= */}
+      <Route path="/" element={<MainLayout />}>
+        {/* HOME */}
+        <Route index element={<HomePage />} />
 
-      {/* ADMIN LOGIN */}
+        {/* OTHER PUBLIC PAGES */}
+        <Route path="about" element={<AboutPage />} />
+        <Route path="courses" element={<CoursesPage />} />
+        <Route path="admission" element={<AdmissionPage />} />
+        <Route path="notices" element={<NoticesPage />} />
+        <Route path="gallery" element={<GalleryPage />} />
+        <Route path="donation" element={<DonationPage />} />
+        <Route path="contact" element={<ContactPage />} />
+      </Route>
+
+      {/* =========================
+          ADMIN AUTH
+      ========================= */}
       <Route path="/admin/login" element={<AdminLoginPage />} />
 
-      {/* DASHBOARD (PROTECTED) */}
+      {/* =========================
+          PROTECTED DASHBOARD
+      ========================= */}
       <Route
         path="/dashboard"
         element={
@@ -41,7 +63,16 @@ const AppRouter = () => {
           </ProtectedRoute>
         }
       >
+        {/* DASHBOARD HOME */}
         <Route index element={<DashboardHomePage />} />
+
+        {/* ADD MORE DASHBOARD ROUTES HERE */}
+
+        {/* Example:
+        <Route path="students" element={<StudentsPage />} />
+        <Route path="teachers" element={<TeachersPage />} />
+        <Route path="settings" element={<SettingsPage />} />
+        */}
       </Route>
 
     </Routes>
